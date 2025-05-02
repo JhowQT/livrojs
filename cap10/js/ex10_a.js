9dc216bf1d6e2588f495457d201708cd2780aee1
JhowQT <jhon.q.torrez@gmail.com>
2025-05-02 16:55:53 -0300

    Criação do esqueleto dos exercicio A do cap 10

---
+++ cap10/js/ex10_a.js
@@ -0,0 +1,5 @@
+/*
+Criar dez imagens de números (de 0 a 9) como se fossem velas de aniversarios e salvá-las na pasta img. 
+Em seguida. eloborar um programa que leia uma idade e insira as imagens correspondente na página conforme
+o número informado. O programa deve permitir idades entre 1 e 120 anos.
+*/
